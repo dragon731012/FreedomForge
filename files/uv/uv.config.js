da7e97e917be15c3198010e446001c35493a14b6
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-08-11 19:40:57 -0500

    Add files via upload

---
+++ files/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/tiw/',
+    bare: 'https://baresw.starttiw.org/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
+};
