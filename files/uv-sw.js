e0b5a7bee7147ab79750cef44ee733b2894db8d8
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-08-11 19:40:12 -0500

    Add files via upload

---
+++ files/uv-sw.js
@@ -0,0 +1,6 @@
+importScripts("/uv/uv.sw.js");
+importScripts('https://arc.io/arc-sw-core.js')
+
+const sw = new UVServiceWorker();
+
+self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
