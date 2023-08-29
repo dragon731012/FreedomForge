6406b6195cf686c2b756b0fd057011a742767c29
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-08-29 13:57:57 +0000

    Delete uv.js

--- files/uv.js
+++
@@ -1,8 +0,0 @@
-importScripts('/uv/uv.bundle.js');
-importScripts('/uv/uv.config.js');
-importScripts('/uv/uv.sw.js');
-importScripts('https://arc.io/arc-sw-core.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
